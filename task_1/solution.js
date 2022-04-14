cb63828cb0c4af24069d866b13be23d77703e718
MakashovPavel <makashov.p@gmail.com>
2022-04-14 10:29:52 +0500

    "task_1 done"

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,14 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName;
+    message = message + ' ';
+    message = message + 'за';
+    message = message + ' ';
+    message = message + productPrice;
+    message = message + ' ';
+    message = message + 'теперь в корзине!';
+    console.log(message);        
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +31,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
+    newValue = newValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +46,8 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
 
     // Задание №1.3. Увеличение суммы в корзине
 
